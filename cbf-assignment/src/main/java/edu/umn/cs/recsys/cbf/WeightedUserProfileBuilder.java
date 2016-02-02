05e16805791eef34c328c1be4e7b06c4bda58553
Zeyuan Shang <zeyuanxy@gmail.com>
2016-02-02 21:57:12 +0800

    part 2 of cbf-assignment is finished. CBF Programming Assignment is finished

--- cbf-assignment/src/main/java/edu/umn/cs/recsys/cbf/WeightedUserProfileBuilder.java
+++ cbf-assignment/src/main/java/edu/umn/cs/recsys/cbf/WeightedUserProfileBuilder.java
@@ -2,6 +2,8 @@ package edu.umn.cs.recsys.cbf;
 
 import it.unimi.dsi.fastutil.longs.Long2DoubleMap;
 import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap;
+import org.grouplens.lenskit.transform.clamp.RatingRangeClampingFunction;
+import org.grouplens.lenskit.vectors.MutableSparseVector;
 import org.lenskit.data.ratings.Rating;
 import org.lenskit.data.history.UserHistory;
 import org.lenskit.util.collections.LongUtils;
@@ -31,8 +33,29 @@ public class WeightedUserProfileBuilder implements UserProfileBuilder {
         // Create a new vector over tags to accumulate the user profile
         Long2DoubleOpenHashMap profile = new Long2DoubleOpenHashMap();
 
-        // TODO Normalize the user's ratings
-        // TODO Build the user's weighted profile
+        // Normalize the user's ratings
+        // Build the user's weighted profile
+        double mean = 0;
+        int count = 0;
+        for (Rating r: history) {
+            if (r.hasValue()) {
+                mean += r.getValue();
+                ++count;
+            }
+        }
+        mean /= count;
+
+        for (Rating r: history) {
+            if (r.hasValue()) {
+                for (Map.Entry<Long, Double> entry : model.getItemVector(r.getItemId()).entrySet()) {
+                    Double newValue = entry.getValue() * (r.getValue() - mean);
+                    if (profile.containsKey(entry.getKey())) {
+                        newValue += profile.get(entry.getKey());
+                    }
+                    profile.put(entry.getKey(), newValue);
+                }
+            }
+        }
 
         // The profile is accumulated, return it.
         // It is good practice to return a frozen vector.
